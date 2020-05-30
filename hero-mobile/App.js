ab1f66df0c67571ebe1b3560be5fc2c1b2684a36
DevSp <contatodevsp@gmail.com>
2020-05-29 22:38:01 -0300

    criando o component de rota e import em app

--- hero-mobile/App.js
+++ hero-mobile/App.js
@@ -1,25 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+
+import Routes from './src/routes'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-    </View>
-  );
+    return (
+        <Routes />
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold'
-  }
-});
